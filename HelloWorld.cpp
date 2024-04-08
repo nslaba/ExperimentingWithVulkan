49b95921d9a18739f2195a154d07430fad73d8d6
Natalie Slaba <slaba.natalie@gmail.com>
2024-04-08 11:18:57 -0700

    doing it right B)

---
+++ HelloWorld.cpp
@@ -0,0 +1,13 @@
+// This is a helloWorld to figuring out Vulkan
+#include <vulkan/vulkan.h>
+#include <iostream>
+
+int main() {
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Vulkan";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+}
